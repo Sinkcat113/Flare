d9d8389e28929c6a3b2de8fd816d3c1180722535
Sophie <132151681+Sinkcat113@users.noreply.github.com>
2024-07-06 22:33:45 -0500

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCozpomyy6Y6H8pSMa4BKBlIIQpi7egRdI",
-    authDomain: "flare-cccd9.firebaseapp.com",
-    databaseURL: "https://flare-cccd9-default-rtdb.firebaseio.com",
-    projectId: "flare-cccd9",
-    storageBucket: "flare-cccd9.appspot.com",
-    messagingSenderId: "528255933039",
-    appId: "1:528255933039:web:487a3546f4681a4c37cb9a"
+    // Your firebase config here
 };
 
 const app = initializeApp(firebaseConfig);
